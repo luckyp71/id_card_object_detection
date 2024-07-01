56c5db1ea82530384b76ca56b46f253dbcb9fd54
Lucky Pratama <luckypratama71@yahoo.com>
2024-07-01 23:19:02 +0700

    adding thread in id_card_detection_api

--- id_card_detection_api/routers/id_detection_router.py
+++ id_card_detection_api/routers/id_detection_router.py
@@ -1,4 +1,6 @@
 import os
+import time
+from concurrent.futures import ThreadPoolExecutor, ProcessPoolExecutor
 from typing import List
 from fastapi import APIRouter, UploadFile, File
 from starlette import status
@@ -7,7 +9,8 @@ from controllers.id_detection_controller import detect_id_document, detect_blur,
 from models.ResponseModel import ResponseModel
 from PIL import Image
 import numpy as np
-
+import threading
+import logging
 
 id_router = APIRouter(
     prefix="/id_detection",
@@ -18,35 +21,47 @@ response_model = ResponseModel(response_code=0, data={})
 
 @id_router.post("/analyze")
 async def analyze_id_detection(file: List[UploadFile] = File(...)):
-    responses=[]
-    for i in file:
-        image = Image.open(i.file)
-        image_arr = np.array(image)
+    responses = []
+    threads = []
+    for f in file:
+        thread = threading.Thread(target=process_image, args=(responses, f,))
+        threads.append(thread)
 
-        bbox = detect_id_document(image_arr)[0]
-        x, y, w, h = bbox
+    for thread in threads:
+        thread.start()
 
-        # ID Card Detection
-        id_image = image_arr[int(y):int(y)+int(h), int(x):int(x)+int(w)]
-        # Blur Detection
-        is_blurry, blur_value = detect_blur(id_image)
-        # Glare Detection
-        has_glare, glare_value = detect_glare(id_image)
-        # Hologram Detection
-        is_hologram = detect_hologram(id_image)
-        # Setup response
-        responses.append(
-                        {"blur":{
-                                "is_blurry": is_blurry.__str__(),
-                                "value": blur_value},
-                           "glare": {
-                               "has_glare": has_glare.__str__(),
-                                "value": int(glare_value)},
-                            "hologram": {
-                                "is_hologram": is_hologram.__str__()
-                            }
-                            })
+    for t in threads:
+        t.join()
+
     response_model.response_code = status.HTTP_200_OK
     response_model.data = responses
     return response_model
 
+def process_image(responses, f):
+    image = Image.open(f.file)
+    image_arr = np.array(image)
+
+    bbox = detect_id_document(image_arr)[0]
+    x, y, w, h = bbox
+
+    # ID Card Detection
+    id_image = image_arr[int(y):int(y) + int(h), int(x):int(x) + int(w)]
+    # Blur Detection
+    is_blurry, blur_value = detect_blur(id_image)
+    # Glare Detection
+    has_glare, glare_value = detect_glare(id_image)
+    # Hologram Detection
+    is_hologram = detect_hologram(id_image)
+
+    time.sleep(0.1)
+    # Setup response
+    responses.append({"blur": {
+            "is_blurry": is_blurry.__str__(),
+            "value": blur_value},
+            "glare": {
+                "has_glare": has_glare.__str__(),
+                "value": int(glare_value)},
+            "hologram": {
+                "is_hologram": is_hologram.__str__()
+            }
+        })
